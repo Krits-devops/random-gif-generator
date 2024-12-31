ca952636cb0c1e211bcae5e77992fee122c5f8c9
Krits-devops <kritagyarm9@gmail.com>
2024-12-30 22:26:38 -0500

    Updated
    Update dependencies and remove unused components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
 export default function App() {
-  return <div>Random GIF</div>;
+  return(
+    <div className="w-full h-screen flex flex-col background">
+      <h1 className="bg-white rounded-sm"> Random Gifs</h1>
+      <div className="flex flex-col">
+        <Random/>
+        <Tag/>
+      </div>
+    </div>
+  )
 }
